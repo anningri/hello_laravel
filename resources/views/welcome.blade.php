4ec6d3a554f01b09895295b4d285a3ad29591ae1
anningri <minganwujian@qq.com>
2019-01-17 00:59:18 +0000

    修改欢迎页面

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Leman</div>
+                <div class="title">Hello Laravel! - by Lemans</div>
             </div>
         </div>
     </body>
